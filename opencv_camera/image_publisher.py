b981e8c3e19e6cfd7f6f6a0ec5ea2b5cc80e5e38
Edward Pie <hackstockpie@gmail.com>
2021-07-29 16:52:34 +0200

    implements image publisher node

---
+++ opencv_camera/image_publisher.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+
+import cv2
+from cv_bridge import CvBridge
+
+
+class ImagePublisher(Node):
+    def __init__(self):
+        super().__init__("image_publisher")
+        self.publisher = self.create_publisher(
+            Image,
+            "image_raw",
+            10
+        )
+
+        self.create_timer(
+            0.1,
+            self.publish_frame
+        )
+
+        self.cap = cv2.VideoCapture(0)
+        self.cv_bridge = CvBridge()
+
+    def publish_frame(self):
+        ret, frame = self.cap.read()
+        if ret:
+            self.publisher.publish(
+                self.cv_bridge.cv2_to_imgmsg(frame)
+            )
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ImagePublisher()
+    try:
+        rclpy.spin(node)
+    except Exception:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
